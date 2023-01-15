62c39c41a53e30036f000f6001a2aca1b64f977b
Caique Araujo <caique@studiopiggly.com.br>
2023-01-15 00:41:50 -0300

    :sparkles: feat(repository): Post with comments repository

---
+++ src/repositories/PostRepository.ts
@@ -0,0 +1,130 @@
+import ApplicationError from '@/exceptions/ApplicationError';
+
+export type TCommentStatus = 'pending' | 'recused' | 'approved';
+
+export interface ICommentRecord {
+	id: string;
+	author: string;
+	content: string;
+	status: TCommentStatus;
+}
+
+export type TPostStatus = 'draft' | 'published' | 'archived';
+
+export interface IPostRecord {
+	id: string;
+	title: string;
+	content: string;
+	status: TPostStatus;
+	comments: ICommentRecord[];
+}
+
+let globalPosts: Record<string, IPostRecord> = {};
+
+export default class PostRepository {
+	static get(id: string) {
+		return globalPosts[id] ?? undefined;
+	}
+
+	static getComment(postId: string, commentId: string) {
+		return globalPosts[postId]?.comments.find(
+			comment => comment.id === commentId
+		);
+	}
+
+	static create(post: Omit<IPostRecord, 'comments'>) {
+		const createdPost = { ...post, comments: [] };
+
+		globalPosts[createdPost.id] = createdPost;
+		return createdPost;
+	}
+
+	static update(
+		id: string,
+		post: Partial<Omit<IPostRecord, 'id' | 'comments'>>
+	) {
+		const found = PostRepository.get(id);
+
+		if (!found) {
+			throw new ApplicationError(
+				500,
+				'CannotUpdatePost',
+				'Post does not exist to be updated.'
+			);
+		}
+
+		const updatedPost = {
+			...found,
+			title: post.title ?? found.title,
+			content: post.content ?? found.content,
+			status: post.status ?? found.status,
+		};
+
+		globalPosts[id] = updatedPost;
+		return updatedPost;
+	}
+
+	static addComment(
+		postId: string,
+		comment: Omit<ICommentRecord, 'status'>
+	) {
+		const found = PostRepository.get(postId);
+
+		if (!found) {
+			throw new ApplicationError(
+				500,
+				'CannotAddComment',
+				'Post does not exist to add comment.'
+			);
+		}
+
+		const createComment: ICommentRecord = {
+			...comment,
+			status: 'pending',
+		};
+
+		found.comments.push(createComment);
+		globalPosts[postId] = found;
+		return createComment;
+	}
+
+	static updateComment(
+		postId: string,
+		commentId: string,
+		comment: Partial<Omit<ICommentRecord, 'id' | 'author'>>
+	) {
+		const found = PostRepository.getComment(postId, commentId);
+
+		if (!found) {
+			throw new ApplicationError(
+				500,
+				'CannotUpdateComment',
+				'Comment does not exist to be updated.'
+			);
+		}
+
+		const updatedComment = {
+			...found,
+			content: comment.content ?? found.content,
+			status: comment.status ?? found.status,
+		};
+
+		globalPosts[postId].comments.forEach((currentComment, idx) => {
+			if (currentComment.id !== updatedComment.id) {
+				return;
+			}
+
+			globalPosts[postId].comments[idx] = updatedComment;
+		});
+
+		return updatedComment;
+	}
+
+	static all() {
+		return Object.values(globalPosts);
+	}
+
+	static fresh() {
+		globalPosts = {};
+	}
+}
---
+++ test/repositories/PostRepository.spec.ts
@@ -0,0 +1,190 @@
+import routes from '@/routes';
+import PostRepository, {
+	ICommentRecord,
+	IPostRecord,
+} from '@/repositories/PostRepository';
+import ApiServer from '@/server/ApiServer';
+import FastifyApplierGroup from '@/server/FastifyApplierGroup';
+import plugins from '@/server/plugins';
+
+beforeAll(async () => {
+	const api = new ApiServer({
+		routes: new FastifyApplierGroup(...routes),
+		plugins: new FastifyApplierGroup(...plugins),
+	});
+
+	await api.bootstrap();
+	PostRepository.fresh();
+});
+
+describe('Post Repository', () => {
+	let createdPost: IPostRecord;
+	let createdComment: ICommentRecord;
+
+	it('should get empty post', () => {
+		expect(PostRepository.all().length).toBe(0);
+	});
+
+	it('can add and get a post', () => {
+		const created = PostRepository.create({
+			id: '1',
+			title: 'My first post',
+			content: 'This is my first post',
+			status: 'draft',
+		});
+
+		const found = PostRepository.get(created.id);
+		expect(found).toStrictEqual(created);
+
+		createdPost = created;
+	});
+
+	it('should get recent created post', () => {
+		const posts = PostRepository.all();
+
+		expect(posts.length).toBe(1);
+		expect(posts[0]).toStrictEqual(createdPost);
+	});
+
+	it('cannot get a post by invalid id', () => {
+		expect(PostRepository.get('unknown')).toBeUndefined();
+	});
+
+	it('can update a post', () => {
+		let updatedPost = PostRepository.update(createdPost.id, {
+			title: 'My new title',
+		});
+
+		expect(updatedPost).toStrictEqual({
+			id: createdPost.id,
+			title: 'My new title',
+			content: createdPost.content,
+			status: 'draft',
+			comments: [],
+		});
+
+		updatedPost = PostRepository.update(createdPost.id, {
+			content: 'My new content',
+		});
+
+		expect(updatedPost).toStrictEqual({
+			id: createdPost.id,
+			title: 'My new title',
+			content: 'My new content',
+			status: 'draft',
+			comments: [],
+		});
+
+		updatedPost = PostRepository.update(createdPost.id, {
+			title: 'My updated title',
+			content: 'This is my updated content, okay?',
+		});
+
+		expect(updatedPost).toStrictEqual({
+			id: createdPost.id,
+			title: 'My updated title',
+			content: 'This is my updated content, okay?',
+			status: 'draft',
+			comments: [],
+		});
+
+		updatedPost = PostRepository.update(createdPost.id, {
+			title: 'My updated title',
+			content: 'This is my updated content, okay?',
+			status: 'published',
+		});
+
+		expect(updatedPost).toStrictEqual({
+			id: createdPost.id,
+			title: 'My updated title',
+			content: 'This is my updated content, okay?',
+			status: 'published',
+			comments: [],
+		});
+	});
+
+	it('cannot update an invalid post id', () => {
+		expect(() => PostRepository.update('unknown', {})).toThrowError();
+	});
+
+	it('should get empty comments', () => {
+		expect(PostRepository.all()[0].comments.length).toBe(0);
+	});
+
+	it('can add a new comment to post', () => {
+		createdComment = PostRepository.addComment(createdPost.id, {
+			id: '1',
+			author: 'John Doe',
+			content: 'This is my first comment',
+		});
+
+		expect(createdComment).toStrictEqual({
+			id: '1',
+			author: 'John Doe',
+			content: 'This is my first comment',
+			status: 'pending',
+		});
+
+		const found = PostRepository.getComment(
+			createdPost.id,
+			createdComment.id
+		);
+
+		expect(found).toStrictEqual(createdComment);
+	});
+
+	it('should get recent created comments', () => {
+		const comments = PostRepository.all()[0].comments;
+
+		expect(comments.length).toBe(1);
+		expect(comments[0]).toStrictEqual(createdComment);
+	});
+
+	it('cannot get a comment by invalid id', () => {
+		expect(
+			PostRepository.getComment(createdPost.id, 'unknown')
+		).toBeUndefined();
+
+		expect(
+			PostRepository.getComment('unknown', 'unknown')
+		).toBeUndefined();
+	});
+
+	it('can update a comment', () => {
+		let updatedComment = PostRepository.updateComment(
+			createdPost.id,
+			createdComment.id,
+			{
+				content: 'This is my new comment',
+			}
+		);
+
+		expect(updatedComment).toStrictEqual({
+			id: createdComment.id,
+			author: 'John Doe',
+			content: 'This is my new comment',
+			status: 'pending',
+		});
+
+		updatedComment = PostRepository.updateComment(
+			createdPost.id,
+			createdComment.id,
+			{
+				status: 'approved',
+			}
+		);
+
+		expect(updatedComment).toStrictEqual({
+			id: createdComment.id,
+			author: 'John Doe',
+			content: 'This is my new comment',
+			status: 'approved',
+		});
+	});
+
+	it('cannot update an invalid post id', () => {
+		expect(() =>
+			PostRepository.updateComment(createdPost.id, 'unknown', {})
+		).toThrowError();
+	});
+});
